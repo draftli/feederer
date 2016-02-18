cf5d623212090a5fef6757ca7d81ae68262829d3
Martin Maillard <self@martin-maillard.com>
2016-02-18 20:07:35 +0100

    Improve python formatting

--- priv/feedparserport.py
+++ priv/feedparserport.py
@@ -7,37 +7,42 @@ import feedparser
 
 from erlport.erlterms import Atom
 
-feedparser.USER_AGENT = "Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) \
-Chrome/50.0.2638.0 Safari/537.36 Draft/Feederer"
+feedparser.USER_AGENT = (
+    'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2638.0 '
+    'Safari/537.36 Draft/Feederer'
+)
 
-all_tags = ['author', 'author_detail', 'base', 'bozo', 'bozo_exception',
-            'cloud', 'comments', 'content', 'contributors', 'created',
-            'created_parsed', 'description', 'docs', 'domain', 'email',
-            'enclosures', 'encoding', 'entries', 'errorreportsto', 'etag',
-            'expired', 'expired_parsed', 'feed', 'generator',
-            'generator_detail', 'headers', 'height', 'href', 'icon', 'id',
-            'image', 'info', 'info_detail', 'label', 'language', 'length',
-            'license', 'link', 'links', 'logo', 'modified', 'name',
-            'namespaces', 'path', 'port', 'protocol', 'published',
-            'published_parsed', 'publisher', 'publisher_detail',
-            'registerProcedure', 'rel', 'rights', 'rights_detail', 'scheme',
-            'source', 'status', 'subtitle', 'subtitle_detail', 'summary',
-            'summary_detail', 'tags', 'term', 'textinput', 'title',
-            'title_detail', 'ttl', 'type', 'updated', 'updated_parsed',
-            'value', 'version', 'width']
+all_tags = [
+    'author', 'author_detail', 'base', 'bozo', 'bozo_exception', 'cloud',
+    'comments', 'content', 'contributors', 'created', 'created_parsed',
+    'description', 'docs', 'domain', 'email', 'enclosures', 'encoding',
+    'entries', 'errorreportsto', 'etag', 'expired', 'expired_parsed', 'feed',
+    'generator', 'generator_detail', 'headers', 'height', 'href', 'icon', 'id',
+    'image', 'info', 'info_detail', 'label', 'language', 'length', 'license',
+    'link', 'links', 'logo', 'modified', 'name', 'namespaces', 'path', 'port',
+    'protocol', 'published', 'published_parsed', 'publisher',
+    'publisher_detail', 'registerProcedure', 'rel', 'rights', 'rights_detail',
+    'scheme', 'source', 'status', 'subtitle', 'subtitle_detail', 'summary',
+    'summary_detail', 'tags', 'term', 'textinput', 'title', 'title_detail',
+    'ttl', 'type', 'updated', 'updated_parsed', 'value', 'version', 'width',
+]
 
 
 def py2erl(term, root):
-    """
-    Converts the big feedparser dict output into a big Erlang tuple
-    """
+    """Converts the big feedparser dict output into a big Erlang tuple."""
+
     if isinstance(term, dict) and root in ['namespaces', 'headers']:
-        return [(py2erl(key, root), py2erl(value, root))
-                for key, value in term.items()]
+        return [
+            (py2erl(key, root), py2erl(value, root))
+            for key, value in term.items()
+        ]
 
     if isinstance(term, dict):
-        return [(Atom(key), py2erl(value, key))
-                for key, value in term.items() if value and key in all_tags]
+        return [
+            (Atom(key), py2erl(value, key))
+            for key, value in term.items()
+            if value and key in all_tags
+        ]
 
     if isinstance(term, tuple):
         return (py2erl(t, root) for t in term)
@@ -58,7 +63,7 @@ def py2erl(term, root):
         return term
 
     if isinstance(term, time.struct_time):
-        return time.strftime("%a, %d %b %Y %H:%M:%S", term)
+        return time.strftime('%a, %d %b %Y %H:%M:%S', term)
 
     # repr the extra stuff, should not happen
     term = term.__repr__()
@@ -66,14 +71,20 @@ def py2erl(term, root):
 
 
 def test(url_filepath_or_string, *args):
-    allowed_kwargs = ['etag', 'modified', 'agent', 'referrer', 'handlers',
-                      'request_headers', 'response_headers']
-    args_dict = dict((str(kwarg), val) for kwarg, val in args[0])
-    feedparser_args = dict((kwarg, val)
-                           for (kwarg, val) in args_dict.iteritems()
-                           if kwarg in allowed_kwargs)
+    allowed_kwargs = [
+        'etag', 'modified', 'agent', 'referrer', 'handlers',
+        'request_headers', 'response_headers',
+    ]
+    args_dict = dict(
+        (str(kwarg), val)
+        for kwarg, val in args[0]
+    )
+    feedparser_args = dict(
+        (kwarg, val)
+        for (kwarg, val) in args_dict.iteritems()
+        if kwarg in allowed_kwargs
+    )
 
-    feed = []
     try:
         feed = feedparser.parse(url_filepath_or_string, **feedparser_args)
     except:
